561fbce6185db911c4399e886f68d8145174b74e
clembesso <dessuane@hotmail.fr>
2018-10-04 17:42:49 +0200

    push react app de base

---
+++ src/header/NavBar.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component{
+    render(){
+        return(
+            <div className = "test">Test</div>
+        )
+    }
+}
+
+export default NavBar;
