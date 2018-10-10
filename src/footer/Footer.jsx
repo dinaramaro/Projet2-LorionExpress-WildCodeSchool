745c92f0e7726fc3d3b7f98f80522a4a061b4896
clembesso <dessuane@hotmail.fr>
2018-10-10 14:56:47 +0200

    footer fonctionnel

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -8,39 +8,41 @@ class Footer extends Component {
             <div>
                 <Container fluid >
                     <Row className='footer'>
-                        <Col lg="3" md ='6' xs="12">
+                        <Col lg="2" md='5' sm="5" xs="8">
                             <ul className="list">
                                 <li className="list-top">Section</li>
-                                <li>Destinations</li>
-                                <li>Expériences</li>
-                                <li>Promotions du mois</li>
+                                <li><a href="/destinations/" className="link">Destinations</a></li>
+                                <li><a href="/experiences/" className="link">Expériences</a></li>
+                                <li><a href="/promotions/" className="link">Promotions du mois</a></li>
                             </ul>
                         </Col>
 
-                        <Col lg="3" md ='6' xs="12">
+                        <Col lg="2" md='5' sm="5" xs="8">
                             <ul className="list">
                                 <li className="list-top">Réseaux sociaux</li>
-                                <li>Facebook</li>
-                                <li>Twitter</li>
+                                <a href="https://www.facebook.com/wildcodeschool/"><img src="http://image.noelshack.com/fichiers/2018/41/3/1539170706-facebook.png" alt="Logo" /></a>
+                                <a href="https://twitter.com/wildcodeschool"><img src="http://image.noelshack.com/fichiers/2018/41/3/1539166418-twitter.png" alt="Logo" /></a>
+                                <a href="https://www.instagram.com/wildcodeschool/?hl=fr"><img src="http://image.noelshack.com/fichiers/2018/41/3/1539170468-instagram.png" alt="Logo" /></a>
                             </ul>
                         </Col>
 
-                        <Col lg="3" md ='6' xs="12">
+                        <Col lg="2" md='5' sm="5" xs="8">
                             <ul className="list">
                                 <li className="list-top">Partenaires</li>
-                                <li>Wild Code School</li>
-                                <li>Office du tourisme</li>
+                                <li><a href="https://wildcodeschool.fr" className="link">Wild Code School</a></li>
+                                <li><a href="http://www.bordeaux.fr/o581/office-de-tourisme-et-des-congres-de-bordeaux-metropole" className="link">Office du tourisme</a></li>
                             </ul>
                         </Col>
-                        <Col lg="3" md ='6' xs="12">
+                        <Col lg="2" md='5' sm="5" xs="8">
                             <ul className="list">
-                                <li className="list-top">Mention légale</li>
+                                <li className="list-top">Mentions légales</li>
+                                <li>© Lorion Express</li>
+                                <li></li>
                             </ul>
                         </Col>
                     </Row>
 
                 </Container>
-
             </div>
 
         );
@@ -48,3 +50,5 @@ class Footer extends Component {
 }
 
 export default Footer;
+
+
