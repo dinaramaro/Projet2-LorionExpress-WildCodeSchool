a5b31ce597c32f05aa1222849b3e66e6f5a46ef5
clembesso <dessuane@hotmail.fr>
2018-10-19 12:19:03 +0200

    Version de présentation contenant aussi la partie admin

---
+++ src/admin/CreationVoyage.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { Button, Form, FormGroup, Label, Input, Container } from 'reactstrap';
+import {Link} from 'react-router-dom';
+
+class CreationVoyage extends React.Component {
+  render() {
+    return (
+        <div>
+            <div className="text-center">
+                <Link to="/admin"><Button className='mr-4 mt-4 mb-4' color="secondary" size="lg">Espace Voyages</Button></Link>
+                <Link to="/adminclient"><Button color="secondary" size="lg">Espace clients</Button></Link>
+                <h2>Créer voyages</h2>
+            </div>
+
+        <Container>
+            <Form >
+                <FormGroup>
+                    <Label for="exampleEmail">Titre</Label>
+                    <Input type="text" name="email" id="exampleEmail" placeholder="Paris" />
+                </FormGroup>
+
+                <FormGroup>
+                    <Label for="exampleEmail">Image</Label>
+                    <Input type="text" name="image" id="exampleEmail" placeholder="url" />
+                </FormGroup>
+
+                <FormGroup>
+                    <Label for="exampleEmail">Description</Label>
+                    <Input type="textarea" name="description" id="exampleEmail" placeholder="Description" />
+                </FormGroup>
+
+                <FormGroup>
+                    <Label for="exampleEmail">Details</Label>
+                    <Input type="textarea" name="details" id="exampleEmail" placeholder="Details" />
+                </FormGroup>
+
+                <FormGroup>
+                    <Label for="exampleEmail">Date de départ</Label>
+                    <Input type="text" name="datededepart" id="exampleEmail" placeholder="12/09/2018" />
+                </FormGroup>
+
+                <FormGroup>
+                    <Label for="exampleEmail">Date de retour</Label>
+                    <Input type="text" name="datederetour" id="exampleEmail" placeholder="15/09/2018" />
+                </FormGroup>
+
+                <FormGroup>
+                    <Label for="exampleEmail">Prix</Label>
+                    <Input type="text" name="prix" id="exampleEmail" placeholder="999" />
+                </FormGroup>
+
+                <FormGroup>
+                    <Label for="exampleEmail">Astuces</Label>
+                    <Input type="textarea" name="astuces" id="exampleEmail" placeholder="Visitez la plage de sable fin" />
+                </FormGroup>
+
+                <FormGroup>
+                    <Label for="exampleEmail">Lieu</Label>
+                    <Input type="text" name="lieu" id="exampleEmail" placeholder="Rome" />
+                </FormGroup>
+
+                <FormGroup>
+                    <Label for="exampleEmail">Type</Label>
+                    <Input type="text" name="type" id="exampleEmail" placeholder="Destination" />
+                </FormGroup>
+                <div className='text-center'>
+                <Button>Créer</Button>
+                </div>
+            </Form>
+        </Container>
+    </div>
+    );
+  }
+}
+
+
+export default CreationVoyage;
--- src/body/ListeVoyage.jsx
+++ src/body/ListeVoyage.jsx
@@ -37,7 +37,7 @@ const cards = [{
   cardtitle: "Paris", 
   cardsubtitle: "Du 10 au 13 Novembre 2018", 
   photo: "https://objects.airfrance.com/FR/common/common/img/tbaf/destinations/PAR/slideshow/PAR-4-16_9-1920x1080.jpg", 
-  cardtext: "La Tour Eiffel, le quartier Montmartre, le musée du Louvre... Découvrez tout le patrimoine historique et culturel d'une ville au mille facettes.", 
+  text: "La Tour Eiffel, le quartier Montmartre, le musée du Louvre... Découvrez tout le patrimoine historique et culturel d'une ville au mille facettes.", 
   prix: "379€",
 }, {
   key:'',
@@ -45,7 +45,7 @@ const cards = [{
   cardtitle: "Barcelone", 
   cardsubtitle: "Du 03 au 06 Mars 2019", 
   photo: "https://www.mhapartments.com/assets/img/offerspages/barcelona-spain-12.jpg", 
-  cardtext: "La Sagrada Familia, La Pedrera, le Parc Güell... Un circuit sur la route des majestueuses réalisations de l'architecte Antoni Gaudi.", 
+  text: "La Sagrada Familia, La Pedrera, le Parc Güell... Un circuit sur la route des majestueuses réalisations de l'architecte Antoni Gaudi.", 
   prix: "429€",
 }, {
   key:'',
@@ -53,7 +53,7 @@ const cards = [{
   cardtitle: "Venise",
   cardsubtitle: "Du 15 au 19 février 2019",
   photo: "https://i.ytimg.com/vi/xplFCLKGArQ/maxresdefault.jpg",
-  cardtext: "Un circuit à la découverte de Venise et ses gondoles, ses canaux, ses ruelles... une ville magique sur l'eau qui ne laissera personne de marbre. ",
+  text: "Un circuit à la découverte de Venise et ses gondoles, ses canaux, ses ruelles... une ville magique sur l'eau qui ne laissera personne de marbre. ",
   prix: "530€",
 }, {
   key:'',
@@ -61,7 +61,7 @@ const cards = [{
   cardtitle: "Thaïlande",
   cardsubtitle: "Du 05 au 20 février 2019",
   photo: "https://dak95nwic4sny.cloudfront.net/73/chiang-mai-and-chiang-rai-40160900-1481624149-ImageGalleryLightboxLarge.jpg",
-  cardtext: "La Thaïlande regorge de lieux merveilleux, découvrez le pays du nord au sud, de Chiang Mai à l'île de Koh Phangan, en passant par Bangkok.",
+  text: "La Thaïlande regorge de lieux merveilleux, découvrez le pays du nord au sud, de Chiang Mai à l'île de Koh Phangan, en passant par Bangkok.",
   prix: "1249€",
 }, {
   key:'',
@@ -69,7 +69,7 @@ const cards = [{
   cardtitle: "Maroc",
   cardsubtitle: "Du 20 au 27 avril 2019",
   photo: "https://www.luxair.lu/sites/default/files/styles/slide_images/public/2018-07/Djemma-El-Fna-Marrakech--iStock-496667645_web_0.jpg?itok=FfVS1VK4",
-  cardtext: "Découvrez le Maroc, ses medinas et ses souks animés au parfum d'épices, ses merveilleux palais, son hospitalité... circuit de Casablanca à Marrakech.",
+  text: "Découvrez le Maroc, ses medinas et ses souks animés au parfum d'épices, ses merveilleux palais, son hospitalité... circuit de Casablanca à Marrakech.",
   prix: "649€",
 }, {
   key:'',
@@ -77,7 +77,7 @@ const cards = [{
   cardtitle: "Croatie",
   cardsubtitle: "Du 10 au 18 Mai 2019",
   photo: "https://www.luxair.lu/sites/default/files/styles/slide_images/public/2018-06/Dubrovnik%2C-Croatia--iStock-171352601_web.jpg?itok=eT5vL82V",
-  cardtext: "Un superbe circuit de Split à Dubrovnik, à travers les îles de la Dalmatie, pour découvrir un pays au patrimoine naturel et historique riche.",
+  text: "Un superbe circuit de Split à Dubrovnik, à travers les îles de la Dalmatie, pour découvrir un pays au patrimoine naturel et historique riche.",
   prix: "760€"
 }
 ];
@@ -96,7 +96,7 @@ class ListeVoyage extends Component {
                     cardtitle = {card.cardtitle}
                     cardsubtitle = {card.cardsubtitle}
                     photo = {card.photo}
-                    carttext = {card.cardtext}
+                    cardtext = {card.text}
                     prix = {card.prix}
                     />
                   )
@@ -115,7 +115,7 @@ class ListeVoyage extends Component {
                     cardtitle = {card.cardtitle}
                     cardsubtitle = {card.cardsubtitle}
                     photo = {card.photo}
-                    carttext = {card.cardtext}
+                    cardtext = {card.text}
                     prix = {card.prix}
                     />
                   )
